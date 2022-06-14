efcc073f70e83c32ba1c2b93d0bcf0050bdb731d
Low0409 <kitagawa0409@icloud.com>
2022-06-14 13:18:47 +0900

    menu追加

---
+++ vue.js
@@ -0,0 +1,351 @@
+
+new Vue({
+    el: '#don',
+    data: {
+        menulist: [
+            {
+                name: 'ロコモコ丼',
+                price: '528',
+                tax_price: '580',
+                img: './img/menu/Booth/don/RokomokoBowl.png',
+                description: 'Fried Pork & egg Rice Bowl',
+            },
+            {
+                name: '月見ソースカツ丼',
+                price: '528',
+                tax_price: '580',
+                img: './img/menu/Booth/don/tukimiBowl.png',
+                description: 'Fried Pork & egg Rice Bowl',
+            },
+            {
+                name: 'カツとじ丼',
+                price: '528',
+                tax_price: '580',
+                img: './img/menu/Booth/don/Pork&EggBowl.png',
+                description: 'Fried Pork & egg Rice Bowl',
+            },
+            {
+                name: '唐揚げ親子丼',
+                price: '528',
+                tax_price: '580',
+                img: './img/menu/Booth/don/Chicken&EggRiceBowl.png',
+                description: 'Fried Pork & egg Rice Bowl',
+            },
+            {
+                name: 'スタミナ丼',
+                price: '528',
+                tax_price: '580',
+                img: './img/menu/Booth/don/StaminaBowl.png',
+                description: 'Fried Pork & egg Rice Bowl',
+            },
+            {
+                name: 'エビピラフ',
+                price: '437',
+                tax_price: '480',
+                img: './img/menu/Booth/don/ShrimpPiraf.png',
+                description: 'Fried Pork & egg Rice Bowl',
+            },
+            {
+                name: '本格炒めチャーハン',
+                price: '437',
+                tax_price: '480',
+                img: './img/menu/Booth/don/ChineeStyleFriedRice.png',
+                description: 'Fried Pork & egg Rice Bowl',
+            },
+        ],
+    },
+})
+new Vue({
+    el: '#curry',
+    data: {
+        menulist: [
+            {
+                name: 'オムハヤシ',
+                price: '619',
+                tax_price: '680',
+                img: './img/menu/Booth/curry/OmHayashi.png',
+                description: 'Fried Pork & egg Rice Bowl',
+            },
+            {
+                name: 'ビーフカレー',
+                price: '437',
+                tax_price: '480',
+                img: './img/menu/Booth/curry/BeefCurry.png',
+                description: 'Fried Pork & egg Rice Bowl',
+            },
+            {
+                name: 'ハヤシライス',
+                price: '437',
+                tax_price: '480',
+                img: './img/menu/Booth/curry/HayashiRice.png',
+                description: 'Fried Pork & egg Rice Bowl',
+            },
+            {
+                name: 'ビーフカレーオムライス',
+                price: '619',
+                tax_price: '680',
+                img: './img/menu/Booth/curry/RiceOmelette&BeefCurry.png',
+                description: 'Fried Pork & egg Rice Bowl',
+            },
+            {
+                name: 'ボルガライス',
+                price: '719',
+                tax_price: '780',
+                img: './img/menu/Booth/curry/BorgaRice.png',
+                description: 'Fried Pork & egg Rice Bowl',
+            },
+            {
+                name: 'たっぷりビーフのドリア',
+                price: '528',
+                tax_price: '580',
+                img: './img/menu/Booth/curry/BeefDoria.png',
+                description: 'Fried Pork & egg Rice Bowl',
+            },
+            {
+                name: 'ケチャップオムライス',
+                price: '564',
+                tax_price: '620',
+                img: './img/menu/Booth/curry/KetchupRiceOmelette.png',
+                description: 'Fried Pork & egg Rice Bowl',
+            },
+        ],
+    },
+})
+new Vue({
+    el: '#plate',
+    data: {
+        menulist: [
+            {
+                name: 'とんかつプレート',
+                price: '619',
+                tax_price: '680',
+                img: './img/menu/Booth/curry/OmHayashi.png',
+                description: 'Pork cutlet Plate',
+            },
+            {
+                name: '唐揚げプレート',
+                price: '619',
+                tax_price: '680',
+                img: './img/menu/Booth/curry/OmHayashi.png',
+                description: 'Deep-fried chiken Plate',
+            },
+            {
+                name: 'ハンバーグプレート',
+                price: '619',
+                tax_price: '680',
+                img: './img/menu/Booth/curry/OmHayashi.png',
+                description: 'Hamburg Plate',
+            },
+            {
+                name: '焼肉プレート',
+                price: '619',
+                tax_price: '680',
+                img: './img/menu/Booth/curry/OmHayashi.png',
+                description: 'Yakiniku Plate',
+            },
+        ],
+    },
+})
+new Vue({
+    el: '#pasta',
+    data: {
+        menulist: [
+            {
+                name: 'カルボナーラ',
+                price: '528',
+                tax_price: '580',
+                img: './img/menu/Booth/Pasta/kalbo.png',
+                description: 'Carbonara',
+            },
+            {
+                name: 'ナポリタン',
+                price: '528',
+                tax_price: '580',
+                img: './img/menu/Booth/Pasta/napoli.png',
+                description: 'Napolitan Spaghetti',
+            },
+            {
+                name: '焼きそば',
+                price: '410',
+                tax_price: '450',
+                img: './img/menu/Booth/Pasta/yakisoba.png',
+                description: 'Fried Noodles',
+            },
+            {
+                name: 'ボロネーゼ',
+                price: '528',
+                tax_price: '580',
+                img: './img/menu/Booth/Pasta/bolo.png',
+                description: 'Bolognese',
+            },
+            {
+                name: 'オムソース焼きそば',
+                price: '591',
+                tax_price: '650',
+                img: './img/menu/Booth/Pasta/omusoba.png',
+                description: 'Fried Noodles Omelette',
+            },
+        ],
+    },
+})
+new Vue({
+    el: '#pan',
+    data: {
+        menulist: [
+            {
+                name: 'ミックスピザ',
+                price: '528',
+                tax_price: '580',
+                img: './img/menu/Booth/Bread&Pizza/mixpizza.png',
+                description: 'Carbonara',
+            },
+            {
+                name: 'ロースカツサンド',
+                price: '500',
+                tax_price: '550',
+                img: './img/menu/Booth/Bread&Pizza/katuSandwitch.png',
+                description: 'Napolitan Spaghetti',
+            },
+            {
+                name: '味噌カツサンド',
+                price: '500',
+                tax_price: '550',
+                img: './img/menu/Booth/Bread&Pizza/katuSandwitch.png',
+                description: 'Fried Noodles',
+            },
+            {
+                name: 'トースト',
+                price: '182',
+                tax_price: '200',
+                img: './img/menu/Booth/Bread&Pizza/toast.png',
+                description: 'Bolognese',
+            },
+            {
+                name: 'ミニサラダ',
+                price: '255',
+                tax_price: '280',
+                img: './img/menu/Booth/Bread&Pizza/salada.png',
+                description: 'Fried Noodles OOmelette',
+            },
+        ],
+    },
+})
+new Vue({
+    el: '#side',
+    data: {
+        menulist: [
+            {
+                name: 'フライドポテト',
+                price: '255',
+                tax_price: '280',
+                img: './img/menu/Booth/side-menu/flyedpoteto.png',
+                description: 'Carbonara',
+            },
+            {
+                name: 'メガポテト',
+                price: '455',
+                tax_price: '500',
+                img: './img/menu/Booth/side-menu/megapoteto.png',
+                description: 'Napolitan Spaghetti',
+            },
+            {
+                name: '若鶏の唐揚げ(6個)',
+                price: '346',
+                tax_price: '380',
+                img: './img/menu/Booth/side-menu/karaage.png',
+                description: 'Deep-fried Chicken',
+            },
+            {
+                name: 'ポテト&唐揚げ',
+                price: '346',
+                tax_price: '380',
+                img: './img/menu/Booth/side-menu/potetokaraage.png',
+                description: 'French Fries & Deep-fried Chicken',
+            },
+            {
+                name: 'ポテト&唐揚げ&ソーセージ',
+                price: '437',
+                tax_price: '480',
+                img: './img/menu/Booth/side-menu/potekarasou.png',
+                description: 'French Fries & Deep-fried Chicken & Sausage',
+            },
+            {
+                name: 'ポテト&ナゲット',
+                price: '319',
+                tax_price: '350',
+                img: './img/menu/Booth/side-menu/poteto&naget.png',
+                description: 'French Fries & Naguet',
+            },
+            {
+                name: 'ハッシュドポテト',
+                price: '228',
+                tax_price: '250',
+                img: './img/menu/Booth/side-menu/hashed.png',
+                description: 'Hashed Potato',
+            },
+            {
+                name: 'ハッシュドポテト&ソーセージ',
+                price: '346',
+                tax_price: '380',
+                img: './img/menu/Booth/side-menu/hashedsou.png',
+                description: 'Hashed Potato & Sausage',
+            },
+            {
+                name: 'チキンナゲット',
+                price: '319',
+                tax_price: '350',
+                img: './img/menu/Booth/side-menu/naget.png',
+                description: 'Chiken Nuggets',
+            },
+            {
+                name: 'たこ焼き',
+                price: '346',
+                tax_price: '380',
+                img: './img/menu/Booth/side-menu/takoyaki.png',
+                description: 'Takoyaki',
+            },
+        ],
+    },
+})
+new Vue({
+    el: '#sweets',
+    data: {
+        menulist: [
+            {
+                name: 'ミックスピザ',
+                price: '528',
+                tax_price: '580',
+                img: './img/menu/Booth/Bread&Pizza/mixpizza.png',
+                description: 'Carbonara',
+            },
+            {
+                name: 'ロースカツサンド',
+                price: '500',
+                tax_price: '550',
+                img: './img/menu/Booth/Bread&Pizza/katuSandwitch.png',
+                description: 'Napolitan Spaghetti',
+            },
+            {
+                name: '味噌カツサンド',
+                price: '500',
+                tax_price: '550',
+                img: './img/menu/Booth/Bread&Pizza/katuSandwitch.png',
+                description: 'Fried Noodles',
+            },
+            {
+                name: 'トースト',
+                price: '182',
+                tax_price: '200',
+                img: './img/menu/Booth/Bread&Pizza/toast.png',
+                description: 'Bolognese',
+            },
+            {
+                name: 'ミニサラダ',
+                price: '255',
+                tax_price: '280',
+                img: './img/menu/Booth/Bread&Pizza/salada.png',
+                description: 'Fried Noodles OOmelette',
+            },
+        ],
+    },
+})
