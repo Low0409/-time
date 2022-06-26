91b4ea098ec7fb88532b136c5d20ca4c8a1cdad3
Low0409 <kitagawa0409@icloud.com>
2022-06-26 17:56:22 +0900

    0626-2

--- dataJS/TOP-data.js
+++ dataJS/TOP-data.js
@@ -1,61 +1,63 @@
-new Vue({
-  el: "#k",
-  data: {
-    ServiceList: [
-      {
-        name: "カラオケ",
-        icon: "./img/icon/karaoke.svg",
-        img: "./img/TOP/1.jpg",
-        path: "./services/karaoke.html",
-      },
-      {
-        name: "ビリヤード",
-        icon: "./img/icon/billiard.svg",
-        img: "./img/TOP/2.jpg",
-        path: "./services/amusement/billiard.html",
-      },
-      {
-        name: "ダーツ",
-        icon: "./img/icon/darts.svg",
-        img: "./img/TOP/3.jpg",
-        path: "./services/amusement/darts.html",
-      },
-      {
-        name: "卓球",
-        icon: "./img/icon/pinpon.svg",
-        img: "./img/TOP/4.jpg",
-        path: "./services/amusement/takkyu.html",
-      },
-      {
-        name: "麻雀格闘俱楽部",
-        icon: "./img/icon/karaoke.svg",
-        img: "./img/TOP/5.jpg",
-        path: "./services/amusement/mahjong.html",
-      },
-      {
-        name: "映画・アニメ",
-        icon: "./img/icon/anime.svg",
-        img: "./img/TOP/6.jpg",
-        path: "./services/net-cafe/video.html",
-      },
-      {
-        name: "コミック・雑誌",
-        icon: "./img/icon/book.svg",
-        img: "./img/TOP/7.jpg",
-        path: "./comic.html",
-      },
-      {
-        name: "ビジネス・勉強",
-        icon: "./img/icon/study.svg",
-        img: "./img/TOP/8.jpg",
-        path: "./services/net-cafe/work_study.html",
-      },
-      {
-        name: "オンラインゲーム",
-        icon: "./img/icon/game.svg",
-        img: "./img/TOP/9.jpg",
-        path: "./services/online.html",
-      },
-    ],
-  },
-});
+const app = Vue.createApp({
+  data(){
+    return{
+      ServiceList: [
+        {
+          name: "カラオケ",
+          icon: "./img/icon/karaoke.svg",
+          img: "./img/TOP/1.jpg",
+          path: "./services/karaoke.html",
+        },
+        {
+          name: "ビリヤード",
+          icon: "./img/icon/billiard.svg",
+          img: "./img/TOP/2.jpg",
+          path: "./services/amusement/billiard.html",
+        },
+        {
+          name: "ダーツ",
+          icon: "./img/icon/darts.svg",
+          img: "./img/TOP/3.jpg",
+          path: "./services/amusement/darts.html",
+        },
+        {
+          name: "卓球",
+          icon: "./img/icon/pinpon.svg",
+          img: "./img/TOP/4.jpg",
+          path: "./services/amusement/takkyu.html",
+        },
+        {
+          name: "麻雀格闘俱楽部",
+          icon: "./img/icon/karaoke.svg",
+          img: "./img/TOP/5.jpg",
+          path: "./services/amusement/mahjong.html",
+        },
+        {
+          name: "映画・アニメ",
+          icon: "./img/icon/anime.svg",
+          img: "./img/TOP/6.jpg",
+          path: "./services/net-cafe/video.html",
+        },
+        {
+          name: "コミック・雑誌",
+          icon: "./img/icon/book.svg",
+          img: "./img/TOP/7.jpg",
+          path: "./comic.html",
+        },
+        {
+          name: "ビジネス・勉強",
+          icon: "./img/icon/study.svg",
+          img: "./img/TOP/8.jpg",
+          path: "./services/net-cafe/work_study.html",
+        },
+        {
+          name: "オンラインゲーム",
+          icon: "./img/icon/game.svg",
+          img: "./img/TOP/9.jpg",
+          path: "./services/online.html",
+        },
+      ],
+    }
+  }
+})
+
