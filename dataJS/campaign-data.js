591a4a09c53b2db0e69aab2c924be3f2ffc34f29
Low0409 <kitagawa0409@icloud.com>
2022-06-27 16:19:09 +0900

    0627-2

---
+++ dataJS/campaign-data.js
@@ -0,0 +1,44 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      campaignList: [
+        {
+          title: "ボルガライスフェア実施中！",
+          img: "./img/news&campaign/06_borga.jpg",
+        },
+        {
+          title: "ベルロード店リニューアルオープン！",
+          img: "./img/news&campaign/04_bell.jpg",
+        },
+        {
+          title: "ボルガライスフェア実施中！",
+          img: "./img/news&campaign/09_issyoku.jpg",
+        },
+        {
+          title: "LINE始めました!",
+          img: "./img/news&campaign/line.jpg",
+        },
+        {
+          title: "GREENチャンネル店内放送中!",
+          img: "./img/news&campaign/gc_2205.jpg",
+        },
+        {
+          title: "【全店】飲食物持ち込みOK!!",
+          img: "./img/news&campaign/09_insyoku.jpg",
+        },
+        {
+          title: "LINE始めました!",
+          img: "./img/news&campaign/line.jpg",
+        },
+        {
+          title: "LINE始めました!",
+          img: "./img/news&campaign/line.jpg",
+        },
+        {
+          title: "LINE始めました!",
+          img: "./img/news&campaign/line.jpg",
+        },
+      ],
+    };
+  },
+});
