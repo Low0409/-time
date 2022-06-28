ab92376632eb2aec756bcb969cb8abb23fc3792f
Low0409 <kitagawa0409@icloud.com>
2022-06-28 15:42:36 +0900

    店舗修正

--- components/data.js
+++ components/data.js
@@ -3,8 +3,8 @@ const app = Vue.createApp({});
 app.component("store-div", {
   props: ["store", "text", "img"],
   template: `
-  <div class="md:flex items-center shadow-2xl py-12 md:px-20 sm:px-14 px-6 rounded-2xl mb-20 sm:mx-8 bg-white mt-10">
-  <div class="md:px-10 sm:px-20">
+  <div class="lg:flex items-center shadow-2xl py-12 md:px-20 sm:px-14 px-6 rounded-2xl mb-20 sm:mx-8 bg-white mt-10">
+  <div class="md:px-10 sm:px-4">
       <img src="../img/logo.JPG" class="mx-auto">
       <h2 class="text-red-800 py-4 text-center text-4xl font-black m-auto">{{store}}</h2>
       <p class="text-gray-700 mb-2 md:mb-6 text-xl mt-4">{{text}}</p>
@@ -30,3 +30,14 @@ Access.component("access-div", {
     <iframe :src="src" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" zoom="1" frameborder="0"></iframe>
   </div>`,
 });
+
+const Flexdiv = Vue.createApp({});
+
+Flexdiv.component("flex-div", {
+  props: ["src", "text"],
+  template: `<div class="sm:flex bg-gray-200 max-w-4xl py-8 px-8 rounded-3xl mb-20 shadow-2xl bg-white">
+  <img :src="src" alt="" class="sm:w-1/2 rounded-lg rounded-3xl">
+  <p class="ml-4 mt-4 align-middle">{{text}}</p>
+  </div>`,
+});
+
