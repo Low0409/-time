1e40a87c53e81e8e22ec09283257178111b1d3bd
Low0409 <kitagawa0409@icloud.com>
2022-07-04 16:36:10 +0900

    店舗を探すページ作成

---
+++ dataJS/store-data.js
@@ -0,0 +1,50 @@
+const store = Vue.createApp({
+    data(){
+      return{
+            storeList: [
+          {
+            name: "長浜",
+            path: "./store/nagahama.html",
+          },
+          {
+            name: "長浜バイパス",
+            path: "./store/bypass.html",
+          },
+          {
+            name: "彦根",
+            path: "./store/hikone.html",
+          },
+          {
+            name: "彦根ベルロード",
+            path: "./store/bellroad.html",
+          },
+          {
+            name: "八日市",
+            path: "./store/youkaiti.html",
+          },
+          {
+            name: "近江八幡",
+            path: "./store/hatiman.html",
+          },
+          {
+            name: "水口",
+            path: "./store/minakuti.html",
+          },
+          {
+            name: "守山",
+            path: "./store/moriyama.html",
+          },
+          {
+            name: "草津",
+            path: "./store/kusathu.html",
+          },
+          {
+            name: "西大津",
+            path: "./store/nishiohthu.html",
+          },
+        ],
+      }
+    }
+  })
+  
+  
