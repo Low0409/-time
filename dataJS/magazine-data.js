edb81d30e4c3570786891138202b06465961479c
Low0409 <kitagawa0409@icloud.com>
2022-08-02 16:56:56 +0900

    雑誌ページ追加 0802-end

--- dataJS/magazine-data.js
+++ dataJS/magazine-data.js
@@ -3,19 +3,19 @@ const magazinelist = Vue.createApp({
     return {
       MagagineList: [
         {
-          name: "新聞",
-          img: "./img/magazine/news_paper.jpg",
-          description: "text",
-        },
-        {
-          name: "ビューン",
-          img: "./img/magazine/viewn.jpg",
-          description: "text",
+          name: "ビューン読み放題スポット",
+          img: "./img/magazine/viewn2.jpg",
+          description: "ビューンは雑誌・マンガの読み放題サービスです。",
         },
         {
           name: "各種雑誌",
-          img: "./img/magazine/viewn.jpg",
-          description: "text",
+          img: "./img/magazine/viewn2.jpg",
+          description: "様々な雑誌を取り揃えております。また雑誌のバックナンバーもご用意しておりますので、調べ物などにもお役立てください。",
+        },
+        {
+          name: "新聞",
+          img: "./img/magazine/news_paper.jpg",
+          description: "新聞もご覧いただけます。",
         },
       ],
     };
